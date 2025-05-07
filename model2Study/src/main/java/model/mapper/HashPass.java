544e09c28d0f716004ef5303ca2b66b1d201aae6
donggonyoo <ddkk8525@gmail.com>
2025-05-07 11:08:27 +0900

    0507

---
+++ model2Study/src/main/java/model/mapper/HashPass.java
@@ -0,0 +1,13 @@
+package model.mapper;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class HashPass {
+	public static void main(String[] args) {
+		String pass = "1233";
+		String hashpw = BCrypt.hashpw(pass, BCrypt.gensalt());
+		System.out.println(pass+":"+hashpw);
+		System.out.println( BCrypt.checkpw(pass, hashpw));
+	}
+
+}
